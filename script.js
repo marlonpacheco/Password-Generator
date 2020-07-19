4bcbc37f4ac5db384eba865e8ba39debc9c2c04a
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-07-18 21:22:30 -0500

    JS Elements and vars set up. Function in-progress

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lengthEl = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
+var specCharEl = confirm("Click OK to include Special characters");
+var numCharEl = confirm("Click OK to include Numberic characters");
+var smallCharEl = confirm("Click OK to include Lowercase characters");
+
+var capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+var specChars = "!@#$%^&*.<>?:;"
+var numChars = "1234567890"
+var smallChars = "abcdefghijklmnopqrstuvwxyz"
 
 // Write password to the #password input
 function writePassword() {
