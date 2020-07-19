95cb3e684412fe658b6461f08b1b0c6faa536dd2
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-07-19 01:06:40 -0500

    added WHILE in length prompt. fixed an error in specChars

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // var lengthEl = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
-  // if (lengthEl < 8 || lengthEl > 128){
-  //   lengthEl
-  // };
+// if (lengthEl < 8 || lengthEl > 128){
+//   lengthEl
+// };
 
 // var specCharEl = confirm("Click OK to include Special characters");
 // var numCharEl = confirm("Click OK to include Numberic characters");
 // var smallCharEl = confirm("Click OK to include Lowercase characters");
 
-var capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+var capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var specChars = "!@#$%^&*.<>?:;"
 var numChars = "1234567890"
 var smallChars = "abcdefghijklmnopqrstuvwxyz"
 
 // Write password to the #password input
 
-function generate (){
-    length = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
-    smallCharEl = confirm("Click OK to include Lowercase characters");
-    capCharsEl = confirm("Click OK to inlcude Uppercase characters");
-    numCharsEl = confirm("Click OK to include Numeric characters");
-    
-    values = "";
-    if (smallCharEl) { values = values.concat(smallChars)};
-    if (capCharsEl) { values = values.concat(capChars)};
-    if (numCharsEl) { values = values.concat(numChars)};
-    if (specCharsEl) { values = value.concat(specChars)};
-  };
+function generate() {
+  //Prompts to determine length of password and kinds of characters in the password//
+  length = prompt("How many characters do you like your password to contain?", "Minimum 8 Max 128");
+  while (length<8){length = prompt("How many characters do you like your password to contain?", "Minimum 8 Max 128")};
 
-    
+  smallCharEl = confirm("Click OK to include Lowercase characters");
+  capCharsEl = confirm("Click OK to inlcude Uppercase characters");
+  numCharsEl = confirm("Click OK to include Numeric characters");
+  specCharsEl = confirm("Click OK to include Special characters")
+
+  //set ups the list of characters based on response above//
+  characters = "";
+  if (smallCharEl) { characters = characters.concat(smallChars) };
+  if (capCharsEl) { characters = characters.concat(capChars) };
+  if (numCharsEl) { characters = characters.concat(numChars) };
+  if (specCharsEl) { characters = characters.concat(specChars) };
+
+
+};
+
+
 
 
 
