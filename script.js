1b0d009b7fc94e54c5a6b919e3f4fc2b0fe6ca2d
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-07-19 00:49:07 -0500

    figured out how to concat the characters based on prompt response

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lengthEl = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
-var specCharEl = confirm("Click OK to include Special characters");
-var numCharEl = confirm("Click OK to include Numberic characters");
-var smallCharEl = confirm("Click OK to include Lowercase characters");
+// var lengthEl = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
+  // if (lengthEl < 8 || lengthEl > 128){
+  //   lengthEl
+  // };
+
+// var specCharEl = confirm("Click OK to include Special characters");
+// var numCharEl = confirm("Click OK to include Numberic characters");
+// var smallCharEl = confirm("Click OK to include Lowercase characters");
 
 var capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
 var specChars = "!@#$%^&*.<>?:;"
@@ -11,13 +15,31 @@ var numChars = "1234567890"
 var smallChars = "abcdefghijklmnopqrstuvwxyz"
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function generate (){
+    length = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
+    smallCharEl = confirm("Click OK to include Lowercase characters");
+    capCharsEl = confirm("Click OK to inlcude Uppercase characters");
+    numCharsEl = confirm("Click OK to include Numeric characters");
+    
+    values = "";
+    if (smallCharEl) { values = values.concat(smallChars)};
+    if (capCharsEl) { values = values.concat(capChars)};
+    if (numCharsEl) { values = values.concat(numChars)};
+    if (specCharsEl) { values = value.concat(specChars)};
+  };
 
-}
+    
+
+
+
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generate);
