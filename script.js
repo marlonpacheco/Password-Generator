68e52f1403be801b2fcce784172390562af8d51e
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-07-19 22:25:19 -0500

    added while loop to passLength. Figured out how to display generated password to text box. Started README

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-// Assignment Code
+// Assignment Codes //
 var generateBtn = document.querySelector("#generate");
-// var lengthEl = prompt("How many characters do you like your password to contain?","Minimum 8 Max 128");
-// if (lengthEl < 8 || lengthEl > 128){
-//   lengthEl
-// };
+var capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specChars = "!@#$%^&*.<>?";
+var numChars = "1234567890";
+var smallChars = "abcdefghijklmnopqrstuvwxyz";
 
-// var specCharEl = confirm("Click OK to include Special characters");
-// var numCharEl = confirm("Click OK to include Numberic characters");
-// var smallCharEl = confirm("Click OK to include Lowercase characters");
-
-var capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var specChars = "!@#$%^&*.<>?:;"
-var numChars = "1234567890"
-var smallChars = "abcdefghijklmnopqrstuvwxyz"
-
-// Write password to the #password input
-
+//Function to generate password//
 function generate() {
+
   //Prompts to determine length of password and kinds of characters in the password//
-  length = prompt("How many characters do you like your password to contain?", "Minimum 8 Max 128");
-  while (length<8){length = prompt("How many characters do you like your password to contain?", "Minimum 8 Max 128")};
+  var passLength = prompt("How many characters do you like your password to contain?", "Minimum 8 Max 128");
+      
+      //loop to meet criteria of minimum and max length//
+      while (passLength < 8 || passLength > 128) {
+      passLength = prompt("How many characters do you like your password to contain?", "Minimum 8 Max 128")
+      };
+      console.log("Length:", passLength)
 
-  smallCharEl = confirm("Click OK to include Lowercase characters");
-  capCharsEl = confirm("Click OK to inlcude Uppercase characters");
-  numCharsEl = confirm("Click OK to include Numeric characters");
-  specCharsEl = confirm("Click OK to include Special characters")
+  // prompts to determine list of character selection//
+  var smallCharEl = confirm("Click OK to include Lowercase characters");
+  var capCharsEl = confirm("Click OK to inlcude Uppercase characters");
+  var numCharsEl = confirm("Click OK to include Numeric characters");
+  var specCharsEl = confirm("Click OK to include Special characters");
 
-  //set ups the list of characters based on response above//
+  //set ups the list of character selection based on response above//
   characters = "";
-  if (smallCharEl) { characters = characters.concat(smallChars) };
-  if (capCharsEl) { characters = characters.concat(capChars) };
-  if (numCharsEl) { characters = characters.concat(numChars) };
-  if (specCharsEl) { characters = characters.concat(specChars) };
+      if (smallCharEl) { characters = characters.concat(smallChars) };
+      if (capCharsEl) { characters = characters.concat(capChars) };
+      if (numCharsEl) { characters = characters.concat(numChars) };
+      if (specCharsEl) { characters = characters.concat(specChars) };
+      console.log("Character Selections:", characters);
 
+  //generation of the suggested password//
+      // password starts with "blank" //
+      password = "";
 
+      // loop to select random letters until passLength is met 
+      for (var i = 1; i <= passLength; i++) {
+        password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)))
+      }
+  console.log("Password generated:", password);
+
+  // writes down the password into #password box 
+  document.querySelector("#password").textContent = password;
 };
 
 
-
-
-
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
+// event listener to generate button
 generateBtn.addEventListener("click", generate);
